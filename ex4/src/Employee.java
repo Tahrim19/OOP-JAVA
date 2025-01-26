32ac3a6b0b5017693f184ea528639b7cd8ea19ec
Tahrim19 <tarimbilal4@gmail.com>
2025-01-27 00:43:38 +0500

    day 4

---
+++ ex4/src/Employee.java
@@ -0,0 +1,58 @@
+public class Employee {
+    private int id;
+    public String firstName;
+    public String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getName(){
+        return firstName+" "+lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+    public int setSalary(){
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public int getAnnualSalary(){
+        return salary*12;
+    }
+
+    public int raiseSalary(int percent) {
+        int currentSalary = getSalary();
+        double raiseAmount = (currentSalary * percent) / 100.0;
+        return (int) (currentSalary + raiseAmount);
+    }
+
+    @Override
+    public String toString(){
+        return "Employee[id="+id+",name="+getName()+",salary="+getSalary()+"]";
+    }
+}
